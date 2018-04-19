50625180fc41147f667ff81af6f88320a4f9d39a
Michal Kuklis <michal.kuklis@gmail.com>
2018-04-19 11:18:45 -0400

    Init package setup

---
+++ index.js
@@ -0,0 +1,3 @@
+// this rule should be removed after we add more util functions
+// eslint-disable-next-line import/prefer-default-export
+export { default as getFullName } from './lib/getFullName';
