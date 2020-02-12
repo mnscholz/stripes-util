8c6f473d329ab0665a1b3fadef09efbf3d3a2aba
Zak Burke <zburke@users.noreply.github.com>
2020-02-12 13:36:11 -0500

    UIIN-589 utility function to escape CQL query values (#29)
    Provide a utility function to escape quotes `"` and backslashes `\` in
    the values inserted into CQL queries.
    
    Refs UIIN-589

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-export { default as getFullName } from './lib/getFullName';
-export { default as exportCsv } from './lib/exportCsv';
 export { default as effectiveCallNumber } from './lib/effectiveCallNumber';
+export { default as escapeCqlValue } from './lib/escapeCqlValue';
+export { default as exportCsv } from './lib/exportCsv';
+export { default as getFullName } from './lib/getFullName';
 export * from './validators';
